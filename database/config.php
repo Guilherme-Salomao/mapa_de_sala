8a8667263618b62e6a00c40653c176afb94b7511
Guilherme Salomão Shorane <64329822+Guilherme-Salomao@users.noreply.github.com>
2026-05-26 15:15:10 -0300

    Uncomment database configuration constants

--- database/config.php
+++ database/config.php
@@ -1,10 +1,10 @@
 ﻿<?php
 define('DB_HOST', 'localhost');
-//define('DB_NAME', 'clinicac_mapa_de_sala');
+
 define('DB_NAME', 'mapa_de_sala');
-//definedefine('DB_USER', 'clinicac_glpi');
+
 define('DB_USER', 'root');
-//define('DB_PASS', 'Gs070284@');
+
 define('DB_PASS', '');
 
 define('MAIL_FROM', 'nao-responder@mapa-de-sala.local');
